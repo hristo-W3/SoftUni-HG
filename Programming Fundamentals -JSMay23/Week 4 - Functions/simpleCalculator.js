4999fdb4032958a5c9d4ff6eb6341024342bb600
hristo-W3 <hristo_valeriev@yahoo.com>
2023-07-29 19:55:30 +0300

    Update simpleCalculator.js

--- Programming Fundamentals -JSMay23/Week 4 - Functions/simpleCalculator.js
+++ Programming Fundamentals -JSMay23/Week 4 - Functions/simpleCalculator.js
@@ -1,7 +1,7 @@
 function calculator(num1,num2,operation){
     let calc = {
         'add': (a,b) => a + b,
-        'substract': (a,b) => a - b,
+        'subtract': (a,b) => a - b,
         'multiply': (a,b) => a * b,
         'divide': (a,b) => a / b
     }
@@ -9,4 +9,4 @@ function calculator(num1,num2,operation){
     return console.log(calc[operation](num1, num2));
 }
 
-calculator(1,2,'divide')
+calculator(50,13,'subtract')

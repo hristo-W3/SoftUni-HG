3350e24c6dff4163317873d110dedbcfeccf59a0
hristo-W3 <hristo_valeriev@yahoo.com>
2023-09-15 21:27:13 +0300

    Create sameNumbers.js

---
+++ JS Advanced - Sep23/Week 1 - Exercise (Syntax, Functions, Statements)/sameNumbers.js
@@ -0,0 +1,18 @@
+function solve(params) {
+    let asChars = String(params);
+    let isSame = true;
+    let sum = 0;
+
+    for (let i = 0; i < asChars.length; i++) {
+        if (asChars[i] !== asChars[0]){
+            isSame = false;
+        }    
+        sum += Number(asChars[i])    
+    }
+
+    console.log(isSame ? "true" : "false");
+    console.log(sum);
+}
+
+solve(2222222)
+solve(1234)

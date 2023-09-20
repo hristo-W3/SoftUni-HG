c6979f684e8d44cb563ad8e7898292f7e9b59788
hristo-W3 <hristo_valeriev@yahoo.com>
2023-09-21 00:40:39 +0300

    Create negativePositiveNumbers.js

---
+++ JS Advanced - Sep23/Week 2 - Lab (Arrays and Nested Arrays)/negativePositiveNumbers.js
@@ -0,0 +1,10 @@
+function negativePositiveNumbers(array){
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        array[i] < 0 ? result.unshift(array[i]) : result.push(array[i]);
+    }
+
+    console.log(result.join('\n'));
+}
+
+negativePositiveNumbers([4, -2, 8, 9])

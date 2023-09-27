3a609397c3066e91971e3a25245674cbfee729c9
hristo-W3 <hristo_valeriev@yahoo.com>
2023-09-27 19:57:24 +0300

    Update magixMatrices.js

--- JS Advanced - Sep23/Week 2 - Exercise (Arrays and Nested Arrays)/magixMatrices.js
+++ JS Advanced - Sep23/Week 2 - Exercise (Arrays and Nested Arrays)/magixMatrices.js
@@ -1,3 +1,34 @@
 function solve(arr){
-    
+    for (let row = 0; row < arr.length - 1; row++) {
+        let sumRowOne = arr[row].reduce((acc,el) => acc + el);
+        let sumRowTwo = arr[row + 1].reduce((acc,el) => acc + el);
+        let sumColOne = 0;
+        let sumColTwo = 0;
+
+        for (let col = 0; col < arr[row].length; col++) {
+            sumColOne += arr[row][col];
+            sumColTwo += arr[row + 1][col];
+        }
+
+        if (sumRowOne !== sumRowTwo || sumColOne !== sumColTwo){
+            console.log(false);
+            return;
+        } 
+    }
+    console.log(true);
 }
+
+solve([[4, 5, 6],
+    [6, 5, 4],
+    [5, 5, 5]]
+   )
+
+solve([[11, 32, 45],
+    [21, 0, 1],
+    [21, 1, 1]]
+   )
+
+solve([[1, 0, 0],
+    [0, 0, 1],
+    [0, 1, 0]]
+   )

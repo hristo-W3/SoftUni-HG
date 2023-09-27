ab8d28d5ce2eca994d4753f081cca59ac7f18dc9
hristo-W3 <hristo_valeriev@yahoo.com>
2023-09-27 19:05:36 +0300

    Update listOfNames.js

--- JS Advanced - Sep23/Week 2 - Exercise (Arrays and Nested Arrays)/listOfNames.js
+++ JS Advanced - Sep23/Week 2 - Exercise (Arrays and Nested Arrays)/listOfNames.js
@@ -1,8 +1,12 @@
 function solve(arr){
-    let result = arr.sort((a,b) => a.localeCompare(b))
+    let result = arr.sort(sortAlpha)
         .forEach((x,i) => {
             console.log(`${i + 1}.${x}`);
     });
+
+    function sortAlpha(textA,textB){
+        return textA.localeCompare(textB)
+    }
 }
 
 solve(["John", "Bob", "Christina", "Ema"])

f26aee129ec860bc75e8253b562d70f941ca53d1
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-05 20:42:16 +0300

    Update assemblyLine.js

--- JS Advanced - Sep23/Week 3 - Lab (Objects and Composition)/assemblyLine.js
+++ JS Advanced - Sep23/Week 3 - Lab (Objects and Composition)/assemblyLine.js
@@ -8,14 +8,14 @@ function createAssemblyLine() {
                     car.temp++;
                 } else if (car.temp > car.settings) {
                     car.temp--;
-                }
+                } 
             };
         },
         hasAudio: (car) => {
             car.currentTrack = {'name': '', 'artist': ''};
             car.nowPlaying = () => {
                 if (car.currentTrack !== null) {
-                    console.log(`Now playing ${car.currentTrack.name} by ${car.currentTrack.artist}`);
+                    console.log(`Now playing '${car.currentTrack.name}' by ${car.currentTrack.artist}`);
                 }
             };
         },
@@ -34,3 +34,29 @@ function createAssemblyLine() {
         }
     };
 }
+
+const assemblyLine = createAssemblyLine();
+
+const myCar = {
+    make: 'Toyota',
+    model: 'Avensis'
+};
+
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
+
+assemblyLine.hasAudio(myCar);
+myCar.currentTrack = {
+    name: 'Never Gonna Give You Up',
+    artist: 'Rick Astley'
+};
+myCar.nowPlaying();
+
+assemblyLine.hasParktronic(myCar);
+myCar.checkDistance(0.4);
+myCar.checkDistance(0.2);
+
+console.log(myCar);

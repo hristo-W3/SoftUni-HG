42967370fa4eeefb9c2a1496f735c140f0c668ca
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-06 19:27:39 +0300

    Update carFactory.js

--- JS Advanced - Sep23/Week 3 - Exercise (Objects and Composition)/carFactory.js
+++ JS Advanced - Sep23/Week 3 - Exercise (Objects and Composition)/carFactory.js
@@ -14,17 +14,6 @@ function carFactory(req) {
         }
     }
 
-    // let carriages = {
-    //     hatchback: {
-    //         type: 'hatchback',
-    //         color: req.color
-    //     },
-    //     coupe: {
-    //         type: 'coupe',
-    //         color: req.color
-    //     }
-    // }
-
     if (req.wheelsize % 2 === 0){
         req.wheelsize--;
     }
@@ -55,22 +44,20 @@ function carFactory(req) {
         },
         wheels: wheelsArr
     };
-    console.log(carObj);
-    console.log(carObj.engine);
-    console.log(carObj.carriage);
-    console.log(carObj.wheels);
+
+    return carObj;
 }
 
-carFactory({ model: 'VW Golf II',
+console.log(carFactory({ model: 'VW Golf II',
 power: 90,
 color: 'blue',
 carriage: 'hatchback',
 wheelsize: 14 }
-)
+));
 console.log("----------------");
-carFactory({ model: 'Opel Vectra',
+console.log(carFactory({ model: 'Opel Vectra',
 power: 110,
 color: 'grey',
 carriage: 'coupe',
 wheelsize: 17 }
-)
+));

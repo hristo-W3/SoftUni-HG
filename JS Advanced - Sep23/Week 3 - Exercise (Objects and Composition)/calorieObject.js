e5f38313ce4f296af259c633616c698c659c0d83
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-06 17:48:47 +0300

    Create calorieObject.js

---
+++ JS Advanced - Sep23/Week 3 - Exercise (Objects and Composition)/calorieObject.js
@@ -0,0 +1,10 @@
+function calorieObject(arr) {
+    let obj = {};
+
+    for (let i = 0; i < arr.length - 1; i+=2) {
+        obj[arr[i]] = Number(arr[i+1]);
+    }
+    console.log(obj);
+}
+
+calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52'])

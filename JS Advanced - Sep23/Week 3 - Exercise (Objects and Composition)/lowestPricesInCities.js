8b1b7fe08d0d6e2264034750b79818ec35daf594
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-06 20:43:28 +0300

    Create lowestPricesInCities.js

---
+++ JS Advanced - Sep23/Week 3 - Exercise (Objects and Composition)/lowestPricesInCities.js
@@ -0,0 +1,29 @@
+function lowestPricesInCities(input) {
+    let products = {};
+
+    for (let each of input) {
+        let [town, product, price] = each.split(' | ');
+        price = Number(price);
+
+        if (!products.hasOwnProperty(product)) {
+            products[product] = {};
+        } 
+
+        if (products[product]["price"] === undefined || products[product]["price"] > price) {
+            products[product]["town"] = town;
+            products[product]["price"] = price;
+        }
+    }
+
+    for(let [product, productData] of Object.entries(products)){
+        console.log(`${product} -> ${productData.price} (${productData.town})`);
+    }
+}
+
+lowestPricesInCities(['Sample Town | Sample Product | 1000',
+'Sample Town | Orange | 2',
+'Sample Town | Peach | 1',
+'Sofia | Orange | 3',
+'Sofia | Peach | 2',
+'New York | Sample Product | 1000.1',
+'New York | Burger | 10'])

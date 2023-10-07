c9f74161d00559e9d32912984faaa3495c41895f
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-07 17:16:45 +0300

    Update storeCatalogue.js

--- JS Advanced - Sep23/Week 3 - Exercise (Objects and Composition)/storeCatalogue.js
+++ JS Advanced - Sep23/Week 3 - Exercise (Objects and Composition)/storeCatalogue.js
@@ -18,5 +18,15 @@ storeCatalogue(['Appricot : 20.4',
 'Boiler : 300',
 'Apple : 1.25',
 'Anti-Bug Spray : 15',
-'T-Shirt : 10']
-)
+'T-Shirt : 10'])
+
+console.log("---------------");
+
+storeCatalogue(['Banana : 2',
+"Rubic's Cube : 5",
+'Raspberry P : 4999',
+'Rolex : 100000',
+'Rollon : 10',
+'Rali Car : 2000000',
+'Pesho : 0.000001',
+'Barrel : 10'])

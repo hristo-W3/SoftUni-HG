c79d10298b5b0c56f156ff48d0f9ae6b6c7243be
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-10 21:18:59 +0300

    Create CollectListItems.js

--- JS Advanced - Sep23/Week 4 - Lab (DOM Introduction)/01.Edit Element/EditElement.js
+++ JS Advanced - Sep23/Week 4 - Lab (DOM Introduction)/01.Edit Element/EditElement.js
@@ -1,5 +1,5 @@
 function editElement(ref,match,replacer) {
-    // TODO
+
     const content = ref.textContent;
     const matcher = new RegExp(match,'g');
     const edited = content.replace(matcher, replacer);

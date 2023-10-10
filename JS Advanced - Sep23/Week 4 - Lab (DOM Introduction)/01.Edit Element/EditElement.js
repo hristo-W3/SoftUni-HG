7d500dcade1296ae888633417f6f8cd5d1aa15eb
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-10 21:12:09 +0300

    Update EditElement.js

--- JS Advanced - Sep23/Week 4 - Lab (DOM Introduction)/01.Edit Element/EditElement.js
+++ JS Advanced - Sep23/Week 4 - Lab (DOM Introduction)/01.Edit Element/EditElement.js
@@ -1,3 +1,7 @@
-function editElement() {
+function editElement(ref,match,replacer) {
     // TODO
+    const content = ref.textContent;
+    const matcher = new RegExp(match,'g');
+    const edited = content.replace(matcher, replacer);
+    ref.textContent = edited;
 }

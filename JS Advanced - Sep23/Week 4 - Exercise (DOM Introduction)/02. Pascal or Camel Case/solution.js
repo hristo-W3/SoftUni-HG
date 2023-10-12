1f25fe2d37bb114367636734ab1116bae820e52b
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-12 19:56:02 +0300

    Update solution.js

--- JS Advanced - Sep23/Week 4 - Exercise (DOM Introduction)/02. Pascal or Camel Case/solution.js
+++ JS Advanced - Sep23/Week 4 - Exercise (DOM Introduction)/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,23 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById('text').value.toLowerCase().split(" "); //.join('')
+  let currentCase = document.getElementById('naming-convention').value;
+  
+  let res = "";
+  switch(currentCase){
+    case "Camel Case": 
+      res += input.shift();
+      for (const each of input) {
+        res += each[0].toUpperCase() + each.slice(1);
+      }
+      break;
+    case "Pascal Case": 
+      for (const each of input) {
+        res += each[0].toUpperCase() + each.slice(1);
+      }
+      break;
+    default: 
+      res = 'Error!';
+      break;
+  }
+  document.getElementById('result').textContent = res;
 }

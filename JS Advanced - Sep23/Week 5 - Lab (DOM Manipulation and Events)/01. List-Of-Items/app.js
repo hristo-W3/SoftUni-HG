611fe0621022b878ec28d229c931ee4158ca6796
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-18 17:52:59 +0300

    Update app.js

--- JS Advanced - Sep23/Week 5 - Lab (DOM Manipulation and Events)/01. List-Of-Items/app.js
+++ JS Advanced - Sep23/Week 5 - Lab (DOM Manipulation and Events)/01. List-Of-Items/app.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    // Assign the input text to a variable
+    let inputText = document.getElementById("newItemText").value;
+    // Create a new li element, where the text will be added
+    let li = document.createElement("li");
+    // Add the text to the li element
+    li.appendChild(document.createTextNode(inputText));
+    // Add the new li element to the existing list of items
+    document.getElementById("items").appendChild(li);
+    // Clearing the input
+    document.getElementById("newItemText").value = '';
 }

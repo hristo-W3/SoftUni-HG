444b54c84a450a986227b8c79752a2467c05a09b
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-18 20:47:22 +0300

    Update app.js

--- JS Advanced - Sep23/Week 5 - Lab (DOM Manipulation and Events)/06. Dynamic-Validation/app.js
+++ JS Advanced - Sep23/Week 5 - Lab (DOM Manipulation and Events)/06. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    const inputElement = document.getElementById('email');
+    const regex = new RegExp('[a-z]+@[a-z]+.[a-z]+');
+
+    inputElement.addEventListener("change", validateEmailFIeld);
+
+    function validateEmailFIeld(e) {
+        if (regex.test(inputElement.value)) {
+            inputElement.className = '';
+        } else {
+            inputElement.className = "error";
+        }
+    }
+
+
 }

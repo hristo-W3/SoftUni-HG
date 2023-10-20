ccc8f883f5ed9648d98e63a620f312c2d4f24cef
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-20 18:57:45 +0300

    Create fibonacci.js

---
+++ JS Advanced - Sep23/Week 5 - Exercise (DOM Manipulation and Events)/fibonacci.js
@@ -0,0 +1,21 @@
+function solve(){
+    let prevNumber = 0;
+    let currentNumber = 1;
+
+    function fib(){
+        let next = prevNumber + currentNumber;
+        prevNumber = currentNumber;
+        currentNumber = next;
+        return prevNumber;
+    }
+    return fib;
+}
+
+let fib = solve();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13

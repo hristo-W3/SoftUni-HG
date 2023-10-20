0ccd0acc15bda718a031e01c507d08c58e5b6ce6
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-20 18:28:05 +0300

    Update script.js

--- JS Advanced - Sep23/Week 5 - Exercise (DOM Manipulation and Events)/01. Sections/script.js
+++ JS Advanced - Sep23/Week 5 - Exercise (DOM Manipulation and Events)/01. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   console.log('TODO:...');
+   const contentRef = document.getElementById("content");
+
+   for (let word of words) {
+      let divRef = document.createElement("div");
+      let pRef = document.createElement("p");
+      pRef.style.display = "none";
+      pRef.textContent = word;
+
+      divRef.addEventListener("click", onClickHandler);
+
+      divRef.appendChild(pRef);
+      contentRef.appendChild(divRef);
+   }
+
+   function onClickHandler(e) {
+      let divRef = e.currentTarget;
+      let pRef = divRef.children[0];
+      pRef.style.display = "block";
+   }
 }

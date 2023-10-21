ef5146074accd1e77f1d122a2efa340f76502047
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-21 11:12:59 +0300

    Update app.js

--- JS Advanced - Sep23/Week 7 - Exercise (Unit Testing)/06.Dynamic-Validation/app.js
+++ JS Advanced - Sep23/Week 7 - Exercise (Unit Testing)/06.Dynamic-Validation/app.js
@@ -1,3 +1,11 @@
 function validate() {
-    // TODO
+    const pattern = new RegExp(/^\S+@\S+\.\S+$/gm);
+    const inputElement  = document.getElementById('email');
+    inputElement.addEventListener('change', onChane);
+    function onChane(ev){
+        ev.target.classList.remove('error');
+        if(!pattern.test(ev.target.value)){
+            ev.target.classList.add('error');
+        }
+    }
 }

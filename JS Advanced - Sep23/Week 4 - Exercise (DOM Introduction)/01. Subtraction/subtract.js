1f9d211b28dd11ecf0eda36b5e646f4cb297d8af
hristo-W3 <hristo_valeriev@yahoo.com>
2023-10-25 21:36:22 +0300

    Update subtract.js

--- JS Advanced - Sep23/Week 4 - Exercise (DOM Introduction)/01. Subtraction/subtract.js
+++ JS Advanced - Sep23/Week 4 - Exercise (DOM Introduction)/01. Subtraction/subtract.js
@@ -1,6 +1,7 @@
 function subtract() {
-    let num1 = Number(document.getElementById('firstNumber').value);
-    let num2 = Number(document.getElementById('secondNumber').value);
+    let num1 = Number(document.getElementById("firstNumber").value);
+    let num2 = Number(document.getElementById("secondNumber").value);
     let result = num1 - num2;
-    document.getElementById('result').textContent = result;
+    document.getElementById("result").textContent = result;
+
 }

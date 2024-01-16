48845997bee7acc74d79b2d0f6de1bf1ac4c0375
hristo-W3 <hristo_valeriev@yahoo.com>
2024-01-16 15:03:30 +0200

    Create solve.js

---
+++ JS Advanced/2024/JS Advanced - Jan24/01. Syntax, Functions and Statements/Lab/02. String Length/solve.js
@@ -0,0 +1,8 @@
+function solve(str1, str2, str3) {
+    let sumLength = str1.length + str2.length + str3.length;
+    let averageLength = Math.floor(sumLength/3);
+    console.log(sumLength);
+    console.log(averageLength); 
+}
+
+solve('pasta', '5', '22.3');

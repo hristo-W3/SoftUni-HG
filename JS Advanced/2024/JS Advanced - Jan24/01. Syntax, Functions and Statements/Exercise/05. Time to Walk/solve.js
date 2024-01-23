f0553a5975624b2e1a3c57e2196ef3b88e6aa419
hristo-W3 <hristo_valeriev@yahoo.com>
2024-01-23 19:14:19 +0200

    Create solve.js

---
+++ JS Advanced/2024/JS Advanced - Jan24/01. Syntax, Functions and Statements/Exercise/05. Time to Walk/solve.js
@@ -0,0 +1,16 @@
+function solve(steps, footprint, speed){
+    let distance = steps * footprint;
+    let speedInSeconds = speed / 3.6;
+    let totalTime = Math.round(distance / speedInSeconds);
+    let rest = Math.floor(distance / 500)
+    totalTime += rest * 60;
+
+    let hours = "0" + Math.floor(totalTime / 3600);
+    let minutes = "0" + Math.floor(totalTime / 60);
+    let seconds = "0" + totalTime % 60;
+
+    console.log(`${hours.slice(-2)}:${minutes.slice(-2)}:${seconds.slice(-2)}`);
+}
+
+solve(4000, 0.60, 5)
+solve(2564, 0.70, 5.5)

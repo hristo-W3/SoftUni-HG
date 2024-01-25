5e145de2d63a81a9e1b28842b25b3e0dc858c589
hristo-W3 <hristo_valeriev@yahoo.com>
2024-01-25 17:07:43 +0200

    Update solve.js

---
+++ JS Advanced/2024/JS Advanced - Jan24/02. Arrays and Nested Arrays/Exercise/07. Sorting Numbers/solve.js
@@ -0,0 +1,32 @@
+function solve(arr){
+    //Sorting the arr elements
+    arr.sort((a,b) => a - b);
+    let n = arr.length;
+
+    //To store modified array
+    let tempArr = new Array(n);
+
+    //Adding numbers from sorted array to new array accordingly
+    let arrIndex = 0;
+
+    //Traverse from beginning to end simultaneously
+    for (let i = 0, j = n - 1; i < n / 2 || j > n / 2; i++, j--) {
+
+        if (arrIndex < n){
+            tempArr[arrIndex] = arr[i];
+            arrIndex++;            
+        }
+
+        if (arrIndex < n){
+            tempArr[arrIndex] = arr[j];
+            arrIndex++;
+        } 
+    }
+
+    //Print the sorted array
+    return tempArr;
+}
+
+console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
+console.log('----------------');
+console.log(solve([22, 9, 63, 3, 2, 19, 54, 11, 21, 18]));

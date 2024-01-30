7976c1069a118d45e96679f39f8affa45519e5cc
hristo-W3 <hristo_valeriev@yahoo.com>
2024-01-30 18:47:25 +0200

    Update solve.js

--- JS Advanced/2024/JS Advanced - Jan24/03. Objects and Composition/Lab/03. City Taxes/solve.js
+++ JS Advanced/2024/JS Advanced - Jan24/03. Objects and Composition/Lab/03. City Taxes/solve.js
@@ -1,11 +1,21 @@
 function cityTaxes(name, population, treasury) {
-    const city = {
+    const result = {
         name,
         population,
-        treasury
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percentage) {
+            this.population += Math.floor(this.population  * percentage / 100)
+        },
+        applyRecession(percentage) {
+            this.treasury -= Math.ceil(this.treasury  * percentage / 100)
+        }
     }
 
-    return city;
+    return result;
 }
 
 const city = 
@@ -24,3 +34,5 @@ city2.collectTaxes();
 console.log(city2.treasury);
 city2.applyGrowth(5);
 console.log(city2.population);
+city2.applyRecession(10);
+console.log(city2.treasury);

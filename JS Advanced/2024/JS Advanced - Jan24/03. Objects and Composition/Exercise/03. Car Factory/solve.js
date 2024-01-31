dd52b76e2a2ecba5102431b0ca5b0b397dfe8504
hristo-W3 <hristo_valeriev@yahoo.com>
2024-01-31 20:24:59 +0200

    Update solve.js

--- JS Advanced/2024/JS Advanced - Jan24/03. Objects and Composition/Exercise/03. Car Factory/solve.js
+++ JS Advanced/2024/JS Advanced - Jan24/03. Objects and Composition/Exercise/03. Car Factory/solve.js
@@ -1,22 +1,52 @@
 function solve(order) {
-    let carObj = {
-        model: order.model,
-        engine: {
-            power: 0,
-            volume: 0
-        },
-        carriage: {
-            type: order.carriage,
-            color: order.color
-        },
-        wheels: []
+    let carObj = {};
+
+    carObj.model = order.model;
+
+    // Object with the different engine types
+    let engineEnum = {
+        "Small engine": { power: 90, volume: 1800 },
+        "Normal engine": { power: 120, volume: 2400 },
+        "Monster engine": { power: 200, volume: 3500 }
+    };
+
+    // Object with the carriage types
+    let carriageEnum = {
+        "hatchback": {type: "hatchback", color: order.color},
+        "coupe": {type: "coupe", color: order.color}
     }
-    console.log(carObj);
+
+    // Assign smallest engine possible, that fits the req for power
+    if (order.power <= 90) {
+        carObj.engine = engineEnum["Small engine"];
+    } else if (order.power <= 120) {
+        carObj.engine = engineEnum["Normal engine"];
+    } else {
+        carObj.engine = engineEnum["Monster engine"];
+    }
+
+    carObj.carriage = carriageEnum[order.carriage];
+
+    if (order.wheelsize % 2 == 0) {
+        order.wheelsize--;
+    }
+
+    carObj.wheels = Array(4).fill(order.wheelsize);
+    
+
+    return carObj;
 }
 
-solve({ model: 'VW Golf II',
+console.log(solve({ model: 'VW Golf II',
 power: 90,
 color: 'blue',
 carriage: 'hatchback',
 wheelsize: 14 }
-)
+));
+console.log('--------------------------');
+console.log(solve({ model: 'Opel Vectra',
+power: 110,
+color: 'grey',
+carriage: 'coupe',
+wheelsize: 17 }
+));

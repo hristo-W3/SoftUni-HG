e21ab13c60afdfaaa5b1c2198f3fe3c189fdbe03
hristo-W3 <hristo_valeriev@yahoo.com>
2024-01-31 20:12:32 +0200

    Update solve.js

--- JS Advanced/2024/JS Advanced - Jan24/03. Objects and Composition/Exercise/03. Car Factory/solve.js
+++ JS Advanced/2024/JS Advanced - Jan24/03. Objects and Composition/Exercise/03. Car Factory/solve.js
@@ -1,3 +1,22 @@
-function solve(params) {
-    
+function solve(order) {
+    let carObj = {
+        model: order.model,
+        engine: {
+            power: 0,
+            volume: 0
+        },
+        carriage: {
+            type: order.carriage,
+            color: order.color
+        },
+        wheels: []
+    }
+    console.log(carObj);
 }
+
+solve({ model: 'VW Golf II',
+power: 90,
+color: 'blue',
+carriage: 'hatchback',
+wheelsize: 14 }
+)

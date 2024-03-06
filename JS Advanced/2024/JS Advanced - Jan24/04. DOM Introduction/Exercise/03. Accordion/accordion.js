470b6e5946c61478e1973548c7f0cc257a0fdbe8
hristo-W3 <hristo_valeriev@yahoo.com>
2024-03-06 18:41:59 +0200

    Update accordion.js

--- JS Advanced/2024/JS Advanced - Jan24/04. DOM Introduction/Exercise/03. Accordion/accordion.js
+++ JS Advanced/2024/JS Advanced - Jan24/04. DOM Introduction/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+    let btn = document.getElementsByClassName('button')[0];
+    const text = document.getElementById('extra');
+    
+    btn.textContent = btn.textContent === "More" ? "Less" : "More";
+
+    if (text.style.display == "") {
+        text.style.display = "none";
+    }
+    text.style.display = text.style.display === "none" ? "block" : "none"; 
 }

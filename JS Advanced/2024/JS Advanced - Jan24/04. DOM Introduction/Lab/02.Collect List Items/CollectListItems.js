b25b557d43ccb21342b29e775a66bb56640e2d71
hristo-W3 <hristo_valeriev@yahoo.com>
2024-03-06 16:39:33 +0200

    Update CollectListItems.js

--- JS Advanced/2024/JS Advanced - Jan24/04. DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
+++ JS Advanced/2024/JS Advanced - Jan24/04. DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
@@ -1,17 +1,12 @@
 function extractText() {
-    // find the list in the page
-    const list = document.getElementById('items');
     let result = [];
+    const list = document.getElementById('items');
+    
     const items = Array.from(list.children);
-    // loop all children
     for (let item of items) {
         result.push(item.textContent);
     }    
-    // read their textContent
-    // create resulting string    
-
-    // find on the page the output textarea
+    
     const textArea = document.getElementById('result');
-    // change it's content to equal the resulting string
     textArea.value = result.join('\n');
 }

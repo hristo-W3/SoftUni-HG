3434f209bee4edbef98b73454cc5fd58862e3130
hristo-W3 <hristo_valeriev@yahoo.com>
2024-03-06 18:42:15 +0200

    Update search.js

--- JS Advanced/2024/JS Advanced - Jan24/04. DOM Introduction/Exercise/04. Search in List/search.js
+++ JS Advanced/2024/JS Advanced - Jan24/04. DOM Introduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,17 @@
 function search() {
-   // TODO
+   let searchText = document.getElementById("searchText").value;
+   let items = Array.from(document.querySelectorAll("#towns li"));
+   let counter = 0;
+
+   items.forEach(item => {
+      if(item.textContent.includes(searchText)) {
+         item.style.fontWeight = "bold";
+         item.style.textDecoration = "underline";
+         return counter++;
+      }
+      item.style.fontWeight = "";
+      item.style.textDecoration = "none";
+   })
+
+   document.getElementById("result").textContent = `${counter} matches found`
 }

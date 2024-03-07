eef87e18f520fb0bf8673215456cbcbbbed52fd8
hristo-W3 <hristo_valeriev@yahoo.com>
2024-03-07 17:54:53 +0200

    Update app.js

--- JS Advanced/2024/JS Advanced - Jan24/05. DOM Manipulations and Events/Lab/03. Add-Delete/app.js
+++ JS Advanced/2024/JS Advanced - Jan24/05. DOM Manipulations and Events/Lab/03. Add-Delete/app.js
@@ -8,8 +8,23 @@ function addItem() {
     const liElement = document.createElement('li');
     liElement.textContent = input.value;
 
+    const deleteBtn = document.createElement('a');
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.href = '#';
+    deleteBtn.addEventListener('click', onDelete)
+    liElement.appendChild(deleteBtn);
+
+
+
     const list = document.getElementById('items');
     list.appendChild(liElement);
 
     input.value = '';
+
+    function onDelete(event) {
+        const deleteBtn = event.target;
+        const liElement = deleteBtn.parentElement;
+        liElement.remove();
+    }
 }
+

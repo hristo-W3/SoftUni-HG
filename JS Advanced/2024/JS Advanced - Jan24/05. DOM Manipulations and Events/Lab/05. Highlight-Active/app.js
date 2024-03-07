68d29ab81b5d592c8c0b58f756c4fcfd0ca49f0e
hristo-W3 <hristo_valeriev@yahoo.com>
2024-03-07 18:54:36 +0200

    Update app.js

--- JS Advanced/2024/JS Advanced - Jan24/05. DOM Manipulations and Events/Lab/05. Highlight-Active/app.js
+++ JS Advanced/2024/JS Advanced - Jan24/05. DOM Manipulations and Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,28 @@
 function focused() {
-    console.log('TODO:...');
+    const sectionElements = document.querySelectorAll("div div");
+    const inputFields = document.getElementsByTagName('input');
+    //const inputFieldsArr = Array.from(document.getElementsByTagName('input'));
+
+    // inputFieldsArr.forEach(input => {
+    //     input.addEventListener('focus', inputFocus);
+    //     input.addEventListener('blur', inputBlur);
+    // })
+
+    for (const input of inputFields) {
+        input.addEventListener('focus', inputFocus);
+        input.addEventListener('blur', inputBlur);
+    }
+
+    function inputFocus(e) {
+        const sectionElement = e.target.parentNode;
+        sectionElement.classList.add("focused");
+        // sectionElement.className = "focused";
+    }
+
+    function inputBlur(e) {
+        const sectionElement = e.target.parentNode;
+        sectionElement.classList.remove("focused")
+        // sectionElement.className = "";
+    }
+
 }

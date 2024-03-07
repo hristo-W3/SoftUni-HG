7c20099d20fde1bb8a4192a610569b078a68ce93
hristo-W3 <hristo_valeriev@yahoo.com>
2024-03-07 19:35:43 +0200

    Update solution.js

--- JS Advanced/2024/JS Advanced - Jan24/05. DOM Manipulations and Events/Exercise/02. Time-Converter/solution.js
+++ JS Advanced/2024/JS Advanced - Jan24/05. DOM Manipulations and Events/Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,37 @@
 function attachEventsListeners() {
+    const daysElement = document.getElementById('days');
+    const hoursElement = document.getElementById('hours');
+    const minutesElement = document.getElementById('minutes');
+    const secondsElement = document.getElementById('seconds');
 
-    console.log('TODO:...');
+    document.getElementById("daysBtn").addEventListener("click", onConvertClick);
+    document.getElementById("hoursBtn").addEventListener("click", onConvertClick);
+    document.getElementById("minutesBtn").addEventListener("click", onConvertClick);
+    document.getElementById("secondsBtn").addEventListener("click", onConvertClick);
+
+    const ratios = {
+        days: 1,
+        hours: 24,
+        minutes: 1440,
+        seconds: 86400
+    }
+
+    function convert(value, unit) {
+        let days = value / ratios[unit];
+        return {
+            days: days,
+            hours: days * ratios.hours,
+            minutes: days * ratios.minutes,
+            seconds: days * ratios.seconds
+        }
+    }
+
+    function onConvertClick(ev) {
+        const input = ev.target.parentElement.querySelector('input[type="text"]');
+        const time = convert(Number(input.value), input.id);
+        daysElement.value = time.days;
+        hoursElement.value = time.hours;
+        minutesElement.value = time.minutes;
+        secondsElement.value = time.seconds;
+    }
 }

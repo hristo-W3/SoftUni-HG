45268661546741954d8df66b85111d77d4634b86
hristo-W3 <hristo_valeriev@yahoo.com>
2024-03-11 17:20:47 +0200

    Update demo.js

--- JS Advanced/2024/JS Advanced - Jan24/06. Advanced Functions/Lab/demo.js
+++ JS Advanced/2024/JS Advanced - Jan24/06. Advanced Functions/Lab/demo.js
@@ -8,8 +8,26 @@ const myObj = {
     sayHello
 }
 
+const secondObj = {
+    name: 'John'
+}
+
 sayHello.call(myObj, 'biology', 6);
 sayHello.apply(myObj, ['biology', 6]);
 
 console.log(Math.max.apply(null,[3,4,5,6]));
 console.log(Math.max(...[3,4,5,6]));
+
+const boundFn = sayHello.bind(secondObj);
+boundFn('art' , 4);
+
+const boundFn2 = sayHello.bind(secondObj, 'art');
+boundFn2(4);
+
+const boundFn3 = sayHello.bind(secondObj, 'art', 4);
+boundFn3();
+
+// Below is equivalent to the bind code above
+const boundFnX = function() {
+    sayHello.call(secondObj, 'art', 4)
+}

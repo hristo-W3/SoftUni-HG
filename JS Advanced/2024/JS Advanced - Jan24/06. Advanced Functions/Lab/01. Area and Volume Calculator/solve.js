b26813e607209bfc66ba58cdb2e92008cfbad938
hristo-W3 <hristo_valeriev@yahoo.com>
2024-03-11 18:07:03 +0200

    Update solve.js

--- JS Advanced/2024/JS Advanced - Jan24/06. Advanced Functions/Lab/01. Area and Volume Calculator/solve.js
+++ JS Advanced/2024/JS Advanced - Jan24/06. Advanced Functions/Lab/01. Area and Volume Calculator/solve.js
@@ -1,5 +1,15 @@
 function solve(areaFn, volFn, input) {
-    //ToDo....
+    const data = JSON.parse(input);
+
+    const result = [];
+
+    for (let fig of data) {
+        result.push({
+            area: areaFn.call(fig),
+            volume: volFn.call(fig)
+        })
+    }
+    return result;
 }
 
 const data1 = `[

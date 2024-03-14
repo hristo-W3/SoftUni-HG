07e62b3c0a4bbc6a8688a57f941190609423dcb8
hristo-W3 <hristo_valeriev@yahoo.com>
2024-03-14 14:52:29 +0200

    Update script.js

--- JS Advanced/2024/JS Advanced - Jan24/06. Advanced Functions/Lab/01_1. Sections (now with Closure)/script.js
+++ JS Advanced/2024/JS Advanced - Jan24/06. Advanced Functions/Lab/01_1. Sections (now with Closure)/script.js
@@ -1,18 +1,38 @@
+// Partial application
+// const createPara = (attr) => createElement('p', attr);
+// const createDiv = (content) => createElement('div', {}, content)
+const createPara = createElement.bind(null, 'p');
+const createDiv = createElement.bind(null, 'div', {});
+
 function create(words) {
-   const container = document.getElementById('content')
+   const container = document.getElementById('content');
+
    for (let word of words) {
-      const div = document.createElement('div');
-      const p = document.createElement('p');
-      p.textContent = word;
+      const p = createPara({ textContent: word });
       p.style.display = 'none';
-      div.appendChild(p);
+      const div = createDiv(p);
       container.appendChild(div);
 
       div.addEventListener('click', show);
 
+      // Closure
       function show() {
          p.style.display = p.style.display == "none" ? "block" : "none";
       }
    }
 }
 
+// Decorator function
+function createElement(type, attr, content) {
+   const element = document.createElement(type);
+
+   if (attr) {
+      Object.assign(element, attr);
+   }
+
+   if (content != undefined) {
+      element.appendChild(content);
+   }
+
+   return element;
+}

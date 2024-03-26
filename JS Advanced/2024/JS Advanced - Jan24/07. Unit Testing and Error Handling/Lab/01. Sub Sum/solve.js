05e1900af3d97cc5aa794f11e47782ed5bea07fa
hristo-W3 <hristo_valeriev@yahoo.com>
2024-03-26 15:24:19 +0200

    Update solve.js

--- JS Advanced/2024/JS Advanced - Jan24/07. Unit Testing and Error Handling/Lab/01. Sub Sum/solve.js
+++ JS Advanced/2024/JS Advanced - Jan24/07. Unit Testing and Error Handling/Lab/01. Sub Sum/solve.js
@@ -1,9 +1,17 @@
-function sumRange(array, start, end) {
-    let sum = 0;
+function subSum(arr, startIndex, endIndex) {
+    if (!Array.isArray(arr)) return NaN;
+    startIndex = startIndex >= 0 ? startIndex : 0
+    endIndex = endIndex < 0 ? 0 :
+        (arr.length - 1 < endIndex) ? arr.length - 1
+            : endIndex;
 
-    for (let i = start; i <= end; i++) {
-        sum += array[i];
-    }
-
-    return sum;
+    return arr.slice(startIndex, endIndex + 1)
+        .map(n => Number(n))
+        .reduce((acc, n) => acc + n, 0);
 }
+
+console.log(subSum([10, 20, 30, 40, 50, 60], 3, 300));
+console.log(subSum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1));
+console.log(subSum([10, 'twenty', 30, 40], 0, 2));
+console.log(subSum([], 1, 2));
+console.log(subSum('text', 0, 2));
